80d4a8b737c4bb1386e18fabf192b849ef6496cf
Prabakaran Chenniappan(UST <291502@ust.com>
2025-07-28 14:59:17 +0530

    CRUD

---
+++ src/test/scala/simulation/apiTest/CRUD.scala
@@ -0,0 +1,32 @@
+package simulation.apiTest
+
+import io.gatling.core.scenario.Simulation
+import io.gatling.http.Predef._
+import io.gatling.core.Predef._
+import io.gatling.core.structure.ScenarioBuilder
+import io.gatling.http.protocol.HttpProtocol
+
+class CRUD extends Simulation {
+  val httpProtocol: HttpProtocol = http.baseUrl("https://petstore.swagger.io")
+  val postScn: ScenarioBuilder = scenario("Post").exec(
+    http("Post").post("/v2/pet")
+      .body(RawFileBody("TestData/pet.json")).asJson
+  )
+  val getScn: ScenarioBuilder = scenario("Get").exec(
+    http("Get").get("/v2/pet/710")
+  )
+  val putScn: ScenarioBuilder = scenario("Put").exec(
+    http("Put").put("/v2/pet")
+      .body(RawFileBody("TestData/updatedPet.json")).asJson
+  )
+  val deleteScn: ScenarioBuilder = scenario("Delete").exec(
+    http("Delete").delete("/v2/pet/710")
+  )
+
+  setUp(
+    postScn.inject(atOnceUsers(1)),
+    getScn.inject(atOnceUsers(1)),
+    putScn.inject(atOnceUsers(1)),
+    deleteScn.inject(atOnceUsers(1))
+  ).protocols(httpProtocol)
+}
